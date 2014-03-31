612ad27f55a2a0662827302e9d9807ac1f6716a9
Mattia Asti <mattia.asti@gmail.com>
2014-03-31 09:27:40 +0100

    Built techhub-london from commit 66adf95 on branch source

---
+++ scripts/53c1f868.main.js
@@ -0,0 +1,1 @@
+$(".logo-intro").removeClass("hidden").addClass("animated flipInY"),$(".jumbo-text").removeClass("hidden").addClass("animated fadeInDown"),$(window).scroll(function(){"use strict";$(".navbar").offset().top>50?$(".navbar-fixed-top").addClass("top-nav-collapse"):$(".navbar-fixed-top").removeClass("top-nav-collapse")});
