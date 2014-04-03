5bb59596e0ee8a8c2b5d393472a098a56c2f3ee9
Mattia Asti <mattia.asti@gmail.com>
2014-04-03 14:29:49 +0100

    moved google analytcs in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,22 @@ $(window).scroll(function() {
     }
 });
 
+// Google Analytics
+(function(b, o, i, l, e, r) {
+    b.GoogleAnalyticsObject = l;
+    b[l] || (b[l] =
+        function() {
+            (b[l].q = b[l].q || []).push(arguments)
+        });
+    b[l].l = +new Date;
+    e = o.createElement(i);
+    r = o.getElementsByTagName(i)[0];
+    e.src = '//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e, r)
+}(window, document, 'script', 'ga'));
+ga('create', 'UA-33525349-3', 'techhub.london');
+ga('send', 'pageview');
+
 if ($('.map').length > 0) {
     var mapOptions = {
         zoom: 16,
